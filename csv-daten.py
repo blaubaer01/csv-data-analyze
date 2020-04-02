9cd5eb46b8d9bfa3dc9698817cd8d3dbfd3c7860
Ricky Helfgen <r_ick@gmx.de>
2020-04-02 09:27:36 +0200

    Einzeldaten aufrufen hinzugefügt

--- csv-daten.py
+++ csv-daten.py
@@ -34,21 +34,75 @@ def file_einlesen(auswahl_datei):
         trennzeichen =','
         dezimalzeichen ='.'
     
-    print(trennzeichen, " ", dezimalzeichen)
+    #print(trennzeichen, " ", dezimalzeichen)
+    #Kopfzeile
+    kopfzeile = input('Gibt es eine Kopfzeile \n j/n \n ?').lower()
+    if kopfzeile == 'j':
+        kopfz = 0
+    elif kopfzeile =='n':
+        kopfz = None
+        
     
     #Dateieinlesen
-    csv_daten=pd.read_csv(auswahl_datei,sep=trennzeichen ,decimal=dezimalzeichen)
-    print(csv_daten)
+    df=pd.read_csv(auswahl_datei,sep=trennzeichen ,decimal=dezimalzeichen, header=kopfz)
+    #print(csv_daten)
+    
+    return(df)
+
+# single data
+def ind_trip_data(df):
+    y=1
+    while y < (len(df)):
+        a = y
+        for i in range(5):
+            print(df.iloc[a,:])
+            a +=1
+        y += 5
+                   
+        end_data = input("Next single Data? type 'enter' for yes or 'n' for no \n?")
+        if (len(df)) <y+5:
+            rest = abs((len(df))-y)
+            print(rest, y)
+            for i in range(rest):
+                print(df.iloc[a,:])
+                a +=1
+            print('Das Ende der Tabelle ist erreicht !')
+            break
+        if end_data.lower() == 'n':
+            break
+        
+
+
+
+
+# Programm-Ablauf
+def main():
+    while True:
+        #choosesource(source)
+        print('#'*70)
+        print('Folgende CSV-Dateien zur zur Auswahl, welche möchten sie auswerten?')
+        csv_daten_im_verzeichnis()    
+    
+        #print(csv_dateien)
+        auswahl_datei = input('Welche Daten einlesen ? :')
+        df=file_einlesen(auswahl_datei)
+         
+    
+        print('#'*70)
+        single_data = input('Would you like to see single Data? y/n \n?')
+        if single_data.lower() != 'n':
+            ind_trip_data(df)
+        
+        restart = input('\nWould you like to restart? Enter y/n.\n?')
+        if restart.lower() != 'y':
+            break
+          
+
 
 
 
 #Hauptprozesse starten
 if __name__ == '__main__':
-    #choosesource(source)
-    print('Folgende CSV-Dateien zur zur Auswahl, welche möchten sie auswerten?')
-    csv_daten_im_verzeichnis()    
+    main()
     
-    #print(csv_dateien)
-    auswahl_datei = input('Welche Daten einlesen ? :')
-    file_einlesen(auswahl_datei)
     
