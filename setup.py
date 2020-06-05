0efe22db76330d714853dfe393508bf97c569187
Ricky Helfgen <r_ick@gmx.de>
2020-06-05 17:48:50 +0200

    include dll files for windows exe file compilation, additional I have modified the setup.py file. To compile an exe file package cx-freeze is needed. See. howto make exe.txt file

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ Thanks to https://www.youtube.com/watch?v=Q8CLcGdr90Q
 """
 
 ###create executable file from csv-data-analyze 
-###dons't run till now ! Problems with the scipy package  
+###Problems with the scipy package!
+###after installing change the filename: lib/scipy/spatial/cKDTree.cp37-win_amd64.pyd in ckdtree.pyd
 
 from cx_Freeze import setup, Executable
 import sys
@@ -24,7 +25,8 @@ if sys.platform == 'win32':
 build_exe_options = {
     "packages": ["os", "sys", "pandas", "numpy", "statsmodels", "matplotlib", "scipy", "seaborn", "outliers", "webbrowser"], 
     "excludes": ["tkinter"],
-    "includes": ["SPC_CPA.py", "L_REG.py", "table_functions.py", "regelkarte.py", "msa.py", "charts.py", "tests.py", "table_calc.py", "rand_data.py"] # <-- Include easy_gui
+    "include_files": ["SPC_CPA.py", "L_REG.py", "table_functions.py", "regelkarte.py", "msa.py", "charts.py", "tests.py", "table_calc.py", "rand_data.py", "mkl_core.dll", "mkl_def.dll", "mkl_intel_thread.dll", "mk_mc.dll", "mkl_mc3.dll", "platforms/"], # <-- Include easy_gui
+    "includes": ["SPC_CPA", "L_REG", "table_functions", "regelkarte", "msa", "charts", "tests", "table_calc", "rand_data"]
 }
 
 #build_exe_options = {"packages": ['os', 'pandas', 'numpy', 'statsmodels', 'matplotlib', 'scipy', 'seaborn','outlier_utils'],
@@ -36,6 +38,6 @@ setup(version = "1.0",
       description = "Analyze CSV - File",
       name = "csv-data-analyze",
       options = {"build_exe": build_exe_options},
-      executables = [Executable(datei, base="Win32GUI")])
+      executables = [Executable(datei)])
 
 print("Fertig!")
