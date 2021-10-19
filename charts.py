a963b7db29d4268b70c39d84e6003efa019d2e67
Ricky Helfgen <r_ick@gmx.de>
2021-10-19 22:56:45 +0200

    add boxplot with observations

--- charts.py
+++ charts.py
@@ -913,14 +913,161 @@ def boxplot(df):
     session_write(log)
     
     
+#######################################################################
+###single boxplot    
+def boxplot_w_dot(df):
+    clear()
+    print('Simple Boxplot \n')
+    werte = df.select_dtypes(exclude=['object'])
+    
+    #
+    anz_col_werte = len(werte.columns)
+        
+    list_columns_werte = []
+    list_number =[]
+    i=1
+    for i in range(anz_col_werte):
+        list_columns_werte.append(werte.columns[i])
+        list_number.append(str(i))
+        print(i, werte.columns[i])
+        i+=1
+    
+    
+    while True:
+        value_column= input('Which value column do you want to see: \n(choose number) \n?')
+        if value_column not in list_number:
+            print('wrong input, try again!')
+        else:
+            break  
+    
+    
+    y = list_columns_werte[int(value_column)]
+    
+    
+    ###toleranzen
+    one_two_sided = input('Tolerance: \n0: no tolerance \n1: both side tolerance \n2: one side ut \n3: one side lt \n(choose number) \n?')
+    
+    notol = '0'
+    ###both side tolerance
+    if one_two_sided == '1':
+        
+        while True:
+            tol = input('upper tolerance , lower tolerance \n(choose point-comma / seperate with float-comma, example:2.2 , 1.9) \n?')
+            if ',' in tol:
+                try:
+                    ut, lt = tol.split(',')
+                    ut = float(ut)
+                    lt = float(lt)
+                    if lt > ut:
+                        print('ut<lt, wrong input!')
+                    else:                    
+                        break
+                except Exception as exception:
+                    print('Wrong input, try again!')
+            else:
+                print('wrong input, separator is missing!, please try again!')
+        
+        print(ut,lt)
+        
+     
+    ###one side tolerance ut
+    elif one_two_sided =='2':
+        
+        
+        while True:
+            ut = input('Upper tolerance: \n(choose point-comma) \n?')
+            ut = float(ut)
+            if not isfloat(ut):
+                print("target mean value is not a number with point-comma, please try again")
+            else:
+                lt = 'none'
+                break
+                
+                
+
+    ###one side tolerance lt
+    elif one_two_sided =='3':
+        
+        while True:
+            lt = input('Lower tolerance: \n(choose point-comma) \n?')
+            lt = float(lt)
+            if not isfloat(lt):
+                print("target mean value is not a number with point-comma, please try again")
+            else:
+                ut = 'none'
+                break
+            
+                
+                
+    else:
+        notol = '1'
+            
+    if notol =='1':
+        sns.boxplot(y=y, data=df,
+            whis=[0, 100], width=.6, palette="vlag")
+        sns.stripplot(y=y, data=df,
+              size=4, color=".3", linewidth=0)
+        plt.show()
+        
+    else:
+        
+    
+        if lt =='none':
+              
+            
+            sns.boxplot(y=y, data=df,
+            whis=[0, 100], width=.6, palette="vlag")
+            sns.stripplot(y=y, data=df,
+              size=4, color=".3", linewidth=0)
+            
+            plt.axhline(y=ut,linewidth=2, color='red')
+            
+            
+            plt.show()
+            
+        elif ut=='none':
+            
+            sns.boxplot(y=y, data=df,
+            whis=[0, 100], width=.6, palette="vlag")
+            sns.stripplot(y=y, data=df,
+              size=4, color=".3", linewidth=0)
+            plt.axhline(y=lt,linewidth=2, color='red')
+            plt.show()
+        
+        
+        else:
+        
+            sns.boxplot(y=y, data=df,
+            whis=[0, 100], width=.6, palette="vlag")
+            sns.stripplot(y=y, data=df,
+              size=4, color=".3", linewidth=0)
+            plt.axhline(y=ut,linewidth=2, color='red')
+            plt.axhline(y=lt,linewidth=2, color='red')
+            
+            plt.show()
+    
+    
+    ###########################################################################
+    ###session-datei
+    
+    fname = 'Boxplot with observations'
+    fvalue = 'Value: ' + y
+    #fbygroup = 'X-Achse: ' + x
+    if notol =='1':
+        tol ='no tolerances set'
+    else:
+        tol = 'Tolerance: ' + str(lt) + ';' + str(ut)
+    
+    log = fname + '\n' + fvalue + '\n' + tol + '\n'
+    session_write(log)
     
     
     
     
     
     
-    #df.boxplot(column=y, widths=0.5)
-    #plt.show()
+    
+    
 
 #######################################################################
 ###boxplot with group
@@ -1083,6 +1230,179 @@ def boxplot_groupby(df):
     log = fname + '\n' + fvalue + '\n' + fbygroup + '\n' + tol + '\n'
     session_write(log)
     
+#######################################################################
+###boxplot with group
+def boxplot_groupby_w_o(df):
+    clear()
+    print('Boxplot with one Group \n')
+    kategorie=df.select_dtypes(include=['object','int'])
+    werte = df.select_dtypes(exclude=['object'])
+    
+    #
+    anz_col_werte = len(werte.columns)
+        
+    list_columns_werte = []
+    list_number=[]
+    i=1
+    for i in range(anz_col_werte):
+        list_columns_werte.append(werte.columns[i])
+        list_number.append(str(i))
+        print(i, werte.columns[i])
+        i+=1
+    
+    
+    while True:
+        value_column= input('Which value column do you want to see: \n(choose number) \n?')
+        if value_column not in list_number:
+            print('wrong input, try again!')
+        else:
+            break  
+    
+    
+    clear()
+    #
+    anz_col_kategorie = len(kategorie.columns)
+        
+    list_columns_kategorie = []
+    list_number=[]
+    i=1
+    for i in range(anz_col_kategorie):
+        list_columns_kategorie.append(kategorie.columns[i])
+        list_number.append(str(i))
+        print(i, kategorie.columns[i])
+        i+=1
+    
+    while True:
+        groupby_column = input('Group by column: \n(choose number) \n?')
+        if groupby_column not in list_number:
+            print('wrong input, try again!')
+        else:
+            break  
+    
+    
+    y = list_columns_werte[int(value_column)]
+    x = list_columns_kategorie[int(groupby_column)]
+    
+    
+    ###toleranzen
+    one_two_sided = input('Tolerance: \n0: no tolerance \n1: both side tolerance \n2: one side ut \n3: one side lt \n(choose number) \n?')
+    
+    notol = '0'
+    ###both side tolerance
+    if one_two_sided == '1':
+        
+        while True:
+            tol = input('upper tolerance , lower tolerance \n(choose point-comma / seperate with float-comma, example:2.2 , 1.9) \n?')
+            if ',' in tol:
+                try:
+                    ut, lt = tol.split(',')
+                    ut = float(ut)
+                    lt = float(lt)
+                    if lt > ut:
+                        print('ut<lt, wrong input!')
+                    else:                    
+                        break
+                except Exception as exception:
+                    print('Wrong input, try again!')
+            else:
+                print('wrong input, separator is missing!, please try again!')
+        
+        print(ut,lt)
+        
+     
+    ###one side tolerance ut
+    elif one_two_sided =='2':
+        
+        
+        while True:
+            ut = input('Upper tolerance: \n(choose point-comma) \n?')
+            ut = float(ut)
+            if not isfloat(ut):
+                print("target mean value is not a number with point-comma, please try again")
+            else:
+                lt = 'none'
+                break
+                
+                
+
+    ###one side tolerance lt
+    elif one_two_sided =='3':
+        
+        while True:
+            lt = input('Lower tolerance: \n(choose point-comma) \n?')
+            lt = float(lt)
+            if not isfloat(lt):
+                print("target mean value is not a number with point-comma, please try again")
+            else:
+                ut = 'none'
+                break
+            
+                
+                
+    else:
+        notol = '1'
+            
+    if notol =='1':
+        sns.boxplot(x=x, y=y, data=df,
+            whis=[0, 100], width=.6, palette="vlag")
+        sns.stripplot(x=x, y=y, data=df,
+              size=4, color=".3", linewidth=0)
+        
+        plt.xticks(rotation=45)    
+        plt.show()
+        
+    else:
+        
+    
+        if lt =='none':
+              
+            sns.boxplot(x=x, y=y, data=df,
+            whis=[0, 100], width=.6, palette="vlag")
+            sns.stripplot(x=x, y=y, data=df,
+              size=4, color=".3", linewidth=0)
+            
+            plt.axhline(y=ut,linewidth=2, color='red')
+            
+            
+            plt.show()
+            
+        elif ut=='none':
+            
+            sns.boxplot(x=x, y=y, data=df,
+            whis=[0, 100], width=.6, palette="vlag")
+            sns.stripplot(x=x, y=y, data=df,
+              size=4, color=".3", linewidth=0)
+            plt.axhline(y=lt,linewidth=2, color='red')
+            plt.xticks(rotation=45)
+            plt.show()
+        
+        
+        else:
+        
+            sns.boxplot(x=x, y=y, data=df,
+            whis=[0, 100], width=.6, palette="vlag")
+            sns.stripplot(x=x, y=y, data=df,
+              size=4, color=".3", linewidth=0)
+            plt.axhline(y=ut,linewidth=2, color='red')
+            plt.axhline(y=lt,linewidth=2, color='red')
+            plt.xticks(rotation=45)
+            plt.show()
+    
+    
+    
+    ###########################################################################
+    ###session-datei
+    
+    fname = 'Boxplot by Group with Observations'
+    fvalue = 'Value: ' + y
+    fbygroup = 'Group by: ' + x
+    if notol =='1':
+        tol ='no tolerances set'
+    else:
+        tol = 'Tolerance: ' + str(lt) + ';' + str(ut)
+    
+    log = fname + '\n' + fvalue + '\n' + fbygroup + '\n' + tol + '\n'
+    session_write(log)
 
 
 ######################################################################
